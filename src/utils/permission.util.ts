1371828bf2675ee931658b59830d9979ca6638e1
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2024-02-06 15:47:01 +0300

    endpoint

--- src/utils/permission.util.ts
+++ src/utils/permission.util.ts
@@ -2,7 +2,7 @@ import {FastifyRequest} from "fastify";
 import {PermissionDocument} from "../types/constants/permissions";
 import {PostTypeId} from "../constants/postTypes";
 import postPermission from "../constants/permissions/post.permission";
-import {EndPoints} from "../constants/endPoints";
+import {Index} from "../constants/endPoints";
 import UserRoles, {UserRoleId} from "../constants/userRoles";
 
 const getPermissionKeyPrefix = (method: string) => {
@@ -22,7 +22,7 @@ const getPostPermission = (req: FastifyRequest) : PermissionDocument => {
     let reqData = req as any;
     let path = req.originalUrl.replace(`/api`, "");
     let method = req.method.toUpperCase();
-    let typeIdKey = path.startsWith(EndPoints.POST_TERM) ? "postTypeId" : "typeId";
+    let typeIdKey = path.startsWith(Index.POST_TERM) ? "postTypeId" : "typeId";
     let typeId: PostTypeId = reqData.query[typeIdKey] ?? reqData.body[typeIdKey] ?? 0;
     let permissionKeyPrefix = getPermissionKeyPrefix(method);
     const postTypeIdKey = Object.keys(PostTypeId).find(key => PostTypeId[key as keyof typeof PostTypeId] === typeId) ?? "";
