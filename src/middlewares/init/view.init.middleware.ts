6fa1e936a779a88d9b994f7b2de163fda7674c1f
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2024-11-12 17:20:50 +0300

    view init middleware

--- src/middlewares/init/view.init.middleware.ts
+++ src/middlewares/init/view.init.middleware.ts
@@ -4,23 +4,25 @@ import {LogMiddleware} from "@middlewares/log.middleware";
 
 const set = async (req: FastifyRequest, reply: FastifyReply) => {
     await LogMiddleware.error(req, reply, async () => {
-        let ip = req.ip;
-        let date = new Date();
-        let _id = (req.session && req.sessionAuth && req.sessionAuth.user) ? req.sessionAuth.user.userId.toString() : "";
+        if(!req.isFromAdminPanel){
+            let ip = req.ip;
+            let date = new Date();
+            let _id = (req.sessionAuth && req.sessionAuth.user) ? req.sessionAuth.user.userId.toString() : "";
 
-        Config.onlineUsers = Config.onlineUsers.filter(onlineUser => Number(date.diffMinutes(onlineUser.updatedAt)) < 10);
+            Config.onlineUsers = Config.onlineUsers.filter(onlineUser => date.diffMinutes(onlineUser.updatedAt) < 10);
 
-        let findIndex = Config.onlineUsers.indexOfKey("ip", ip);
-        if(findIndex > -1){
-            Config.onlineUsers[findIndex].updatedAt = date;
-            Config.onlineUsers[findIndex]._id = _id;
-        }else {
-            Config.onlineUsers.push({
-                ip: ip,
-                createdAt: date,
-                updatedAt: date,
-                _id: _id
-            })
+            let findIndex = Config.onlineUsers.indexOfKey("ip", ip);
+            if(findIndex > -1){
+                Config.onlineUsers[findIndex].updatedAt = date;
+                Config.onlineUsers[findIndex]._id = _id;
+            }else {
+                Config.onlineUsers.push({
+                    ip: ip,
+                    createdAt: date,
+                    updatedAt: date,
+                    _id: _id
+                })
+            }
         }
     });
 }
