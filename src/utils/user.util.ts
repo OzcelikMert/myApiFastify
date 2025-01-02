94335659360b59bc217a96eaacc25bd78ae3e3a3
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2025-01-02 07:14:44 +0300

    new encoder for User Password

--- src/utils/user.util.ts
+++ src/utils/user.util.ts
@@ -2,8 +2,17 @@ import Crypto from 'crypto';
 import { Config } from '@configs/index';
 
 const encodePassword = (password: string): string => {
+  const passwordSaltArray = Config.passwordSalt.split('');
+  const passwordArray = password.split('');
+  let encryptedPassword = "";
+  let passwordSaltIndex = 0;
+  for (let i = 0; i < passwordArray.length; i++) {
+    encryptedPassword += passwordArray[i];
+    encryptedPassword += passwordSaltArray[passwordSaltIndex];
+    passwordSaltIndex = passwordSaltIndex < passwordSaltArray.length ? passwordSaltIndex + 1 : 0;
+  }
   return Crypto.createHash('sha256')
-    .update(Config.passwordSalt + password)
+    .update(encryptedPassword)
     .digest('hex');
 };
 
